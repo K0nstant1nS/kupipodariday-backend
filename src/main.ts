b184cbef39efcb2e4797871fb206b3bcc8ca7f7a
Константин Cоловьев <K1kollov@gmail.com>
2023-08-14 20:02:38 +0300

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Название вашего API сервиса')
+    .setDescription('Назначение API сервиса (описание)')
+    .setVersion('1.0')
+    .build();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
